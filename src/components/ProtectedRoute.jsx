4d87fc4f124ae55167295e287f5d48e3ae0f1929
Danceny Karla <dancenykarla@gmail.com>
2024-12-05 12:02:24 +0100

    protected route

---
+++ src/components/ProtectedRoute.jsx
@@ -0,0 +1,9 @@
+import { Navigate } from "react-router-dom";
+
+function ProtectedRoute({ children }) {
+  const token = localStorage.getItem("authToken");
+
+  return token ? children : <Navigate to="/login" />;
+}
+
+export default ProtectedRoute;
